81a549c92c201d4d29324273a6694c0470bfa29f
Liamdev631 <liambury@outlook.com>
2023-05-29 04:21:25 -0400

    Added a graphical normalization test

--- lif.py
+++ lif.py
@@ -2,33 +2,36 @@ import torch
 import torch.nn as nn
 
 class LIFLayer(nn.Module):
-    def __init__(self, weights, dt=1e-2, threshold=1.0, tau_v=1e-2, tau_inhibitory=1e-2, inhibitory_scale=0.1):
+    def __init__(self, weights, device='cpu', dt=1e-3, threshold=1.0, tau_v=20e-3, tau_inhibitory=60e-3, inhibitory_scale=10, R=100):
         super(LIFLayer, self).__init__()
-        self.weights = weights
+        self.weights = weights.to(device)
         self.dt = dt
         self.threshold = threshold
-        self.tau_v = tau_v
-        self.inhibitory_scale = inhibitory_scale
-        self.tau_inhibitory = tau_inhibitory
+        self.R = R
+        self.device = device
 
-        self.state = torch.zeros(weights.shape[0])
-        self.mean_activation = torch.zeros(1)
+        self.v = torch.zeros(size=(weights.shape[0],), device=device)
+        self.tau_v = tau_v
+
+        self.activation = torch.zeros(size=(1,), device=device)
+        self.tau_activation = tau_inhibitory
+        self.inhibitory_scale = inhibitory_scale
 
     def forward(self, x):
         # Calculate v
         x = x * self.weights
-        dv_dt = -self.state / self.tau_v + x - self.mean_activation
-        self.state += dv_dt * self.dt
+        dv_dt = -(self.v / self.tau_v) + x * self.R - self.activation
+        self.v += dv_dt * self.dt
 
         # Check for spiking activity
-        spikes = self.state > self.threshold
+        spikes = self.v > self.threshold
 
         # Update astrocyte state
-        d_inhibition = -self.mean_activation * self.dt / self.tau_inhibitory
-        self.mean_activation += d_inhibition + torch.sum(spikes.int()) * self.inhibitory_scale
+        self.activation += torch.sum(spikes).float() * self.inhibitory_scale
+        self.activation -= self.activation * self.dt / self.tau_activation
 
-        # clamp
-        self.state[spikes] = 0
-        self.state[self.state < 0] = 0
+        # Clamp
+        self.v[spikes] = 0
+        self.v[self.v < 0] = 0
 
         return spikes
